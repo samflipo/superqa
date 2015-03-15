01176afa822f10683a0525f21c539fe49c6424aa
Samuel Weldemariam <weldemariam@apple.com>
2015-03-15 09:25:41 -0700

    Cleaned up the styling

--- app/routes/students.js
+++ app/routes/students.js
@@ -1,5 +1,6 @@
 var Student = require('../models/student.js');
 var Payment = require('../models/payment.js');
+var Account = require('../models/account.js');
 var moment = require('moment');
 
 exports.index = function(req, res){
@@ -14,8 +15,10 @@ exports.fresh = function(req, res){
 
 exports.show = function(req, res){
   Student.findById(req.params.id, function(student){
-    Payment.findByStudentId(student._id.toString(), function(payments, bal){
-      res.render('accounts/students/show', {title: 'Student Account', payments: payments, bal: bal, student: student, moment:moment});
+    Account.findById(student.accountId, function(account){
+      Payment.findByStudentId(student._id.toString(), function(payments, bal){
+        res.render('accounts/students/show', {title: 'Student Account', account: account, payments: payments, bal: bal, student: student, moment:moment});
+      });
     });
   });
 };
