032757c0529dac151ebc84bee781dda929d2afff
Samuel Weldemariam <weldemariam@apple.com>
2015-03-22 09:09:06 -0700

    redis store host and port defined

--- app/app.js
+++ app/app.js
@@ -36,8 +36,7 @@ app.use(methodOverride(function(req, res){
 }));
 app.use(cookieParser());
 app.use(session({
-//  store: new RedisStore({host: 'localhost', port: 6379}),
-  store: new RedisStore({url: config.REDISCLOUD_URL})
+  store: new RedisStore({host: 'localhost', port: 6379}),
   secret: 'change-this-to-a-super-secret-message',
   resave: false,
   saveUninitialized: true,
